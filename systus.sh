7b0e168b01dd77e93e86987cbd72cea542f94d58
geraldelorm <gerald.elorm.gbagbe@gmail.com>
2024-07-02 02:04:50 +0000

    initial plan

---
+++ systus.sh
@@ -0,0 +1,14 @@
+# check cpu usage
+
+# check memory usage
+
+# check disk usage
+
+# check network load 
+
+# check system uptime
+
+
+# print stats/sys-status to console 
+# refetch and update stats/sys-status every few seconds
+
